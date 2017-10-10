2102707cf2aaf0353f1e321c9a0db090d348dcad
Julian <jarangol@eafit.edu.co>
2017-10-09 21:13:09 -0500

    removing new view route

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :ram_uses,  only: [:index,:show, :new, :create]
+  resources :ram_uses,  only: [:index,:show, :create]
   root to: 'ram_uses#index'
 end
