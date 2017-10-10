5afbc0cfa4d1fffacb7e5252e17d3a13bed0c270
Julian <jarangol@eafit.edu.co>
2017-10-09 20:34:25 -0500

    deploying

---
+++ config/routes.rb
@@ -0,0 +1,5 @@
+Rails.application.routes.draw do
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :ram_uses,  only: [:index,:show, :new, :create]
+  root to: 'ram_uses#index'
+end
